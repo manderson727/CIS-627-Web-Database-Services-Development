1e7b428f4afc08c672aa5196e5a99d7a8d665f44
manderson727 <manderson_311@yahoo.com>
2017-07-02 18:28:01 -0400

    Week6

--- week1/tango_with_django_project/rango/urls.py
+++ week1/tango_with_django_project/rango/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
     # url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/', views.restricted, name='restricted'),
     # url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^like/$', views.like_category, name='like_category'),
 ]
